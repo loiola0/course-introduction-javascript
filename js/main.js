5b1245ebce85fa7f3dff97df34b50035594cf35c
loiola0 <victorloiola@alu.ufc.br>
2020-05-09 13:03:08 -0300

    quase concluíndo o curso

--- js/main.js
+++ js/main.js
@@ -1,5 +1,71 @@
 
 
+
+function validaIdade(idade){
+  
+  if(idade>=18){
+    return true;
+  }else{
+    return false;
+  }
+  
+}
+
+var idade = prompt("Qual sua idade");
+
+console.log(validaIdade(idade));
+
+
+/*
+function soma(n1,n2){
+    return n1+n2;
+}
+
+console.log(soma(7,3));
+
+
+function setReplace(frase,trocar,novo){
+
+    return frase.replace(trocar,novo);
+}
+
+
+console.log(setReplace('Vai Argentina','Vai','Perdeu'));
+
+
+
+
+var d = new Date();
+
+alert(d.getMonth()+1);
+
+
+var count;
+
+    for(count=0;count<=5;count++){
+        console.log(count);
+    }
+
+
+var count  = 0;
+
+    while(count < 5){
+        console.log(count);
+        count = count+1;
+    }
+
+
+var idade = prompt('Qual a sua idade?');
+
+    if(idade >= 18){
+        alert('maior de idade');
+    }else{
+        alert('menor de idade !');
+    }
+
+
+
+
 var frutas = [
     {
     nome:"Banana",
@@ -17,7 +83,7 @@ var frutas = [
 
 console.log(frutas[1].nome);
 
-/*
+
 var lista = ['pêra','maçã','goiaba'];
 
 lista.push('uva');
