6f1b5781cf645cb29a4715a3c834f97842188887
loiola0 <victorloiola@alu.ufc.br>
2020-05-09 12:07:21 -0300

    primeira aula cocnluída

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var nome = 'Victor Gabriel';
+var idade = 19;
+var idade2 = 20;
+
+//alert(`Bem Vindo ${nome} você possue ${idade} anos!`);
+
+//alert(idade+idade2);
+
+console.log(idade+idade2);
+//substitui e deixa a string maiuscula.
+console.log(nome.replace('Gabriel','Martins').toUpperCase());
