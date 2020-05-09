af6275caf512cc1add3dce7f048cb5a516e58888
loiola0 <victorloiola@alu.ufc.br>
2020-05-09 12:23:03 -0300

    1ª e 2ª aulas cocnluídas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 
+
+var frutas = [
+    {
+    nome:"Banana",
+    cor:"amarela"
+    },
+    {
+    nome:"Maçã",
+    cor:"vermelha"  
+    },
+    {
+    nome:"Cajarana",
+    cor:"amarela"  
+    }
+];
+
+console.log(frutas[1].nome);
+
+/*
+var lista = ['pêra','maçã','goiaba'];
+
+lista.push('uva');
+
+//lista.pop();
+
+console.log(lista.toString());
+
+console.log(lista.join('|'));
+
+
+
+
 var nome = 'Victor Gabriel';
 var idade = 19;
 var idade2 = 20;
@@ -10,3 +42,4 @@ var idade2 = 20;
 console.log(idade+idade2);
 //substitui e deixa a string maiuscula.
 console.log(nome.replace('Gabriel','Martins').toUpperCase());
+*/
